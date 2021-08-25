16af08302f3fe3b3caca1e76727dfa11a37695a8
fssroxx <fssroxx@gmail.com>
2021-08-26 00:17:24 +0300

    синтаксическая ошибка)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,26 @@
-import React, {useContext} from 'react';
+import React, { useContext } from "react";
 import MyInput from "../components/UI/input/MyInput";
 import MyButton from "../components/UI/button/MyButton";
-import {AuthContext} from "../context";
+import { AuthContext } from "../context";
 
 const Login = () => {
-    const {setIsAuth} = useContext(AuthContext);
+  const { setIsAuth } = useContext(AuthContext);
 
-    const login = event => {
-        event.preventDefault();
-        setIsAuth(true);
-        localStorage.setItem('auth', 'true');
-    }
-    return (
-        <div>
-            <h1>Страница для логина</h1>
-            <form onSubmit={login}>
-                <MyInput type="text" placeholder="введите логин"/>
-                <MyInput type="password" placeholder="введите пороль"/>
-                <MyButton>Войти</MyButton>
-            </form>
-        </div>
-    );
+  const login = (event) => {
+    event.preventDefault();
+    setIsAuth(true);
+    localStorage.setItem("auth", "true");
+  };
+  return (
+    <div>
+      <h1>Страница для логина</h1>
+      <form onSubmit={login}>
+        <MyInput type="text" placeholder="введите логин" />
+        <MyInput type="password" placeholder="введите пароль" />
+        <MyButton>Войти</MyButton>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
